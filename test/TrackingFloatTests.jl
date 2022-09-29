441ddcbd4207ac696e76478be5ab0d04be0a7330
pantonomia <114500973+pantonomia@users.noreply.github.com>
2022-09-29 09:25:41 +0200

    Update TrackingFloatTests.jl

--- test/TrackingFloatTests.jl
+++ test/TrackingFloatTests.jl
@@ -3,7 +3,7 @@ using Test
 using TrackingFloat
 import Base: +, *, -, / , promote, promote_rule, sqrt, qr, <, cholesky, zero 
 
-#test basic operators for our new type
+#test basic operators +, -, *, / for tracking floats
 
 @test v     == TrackingFloat(4,3)           # which we test using the macro @test
 @test v*v   == TrackingFloat(16, 4) 
@@ -19,26 +19,27 @@ At = TrackingFloat.(A)
 bt = TrackingFloat.(b) 
 v = A*b 
 vt = At*bt 
-value.(vt) 
-v - value.(vt) 
+
 @test maximum(abs, v - value.(vt)) < sqrt(eps()) 
 
 # Is promotion working?
 @test TrackingFloat(1.0, 0) + 2.0 == TrackingFloat(3, 2) 
 
 #solve linear system with backsolve with your TrackingFloat
-AA = A*A' 
-AAt = TrackingFloat.(AA) 
+AA = A*A'  # Create Positive definite matrix
+AAt = TrackingFloat.(AA) # Convert to TrackingFloat matrix
+
+#solve linear system with backsolve with your trackingfloat
 sol1 = AAt.\bt
 value.(sol1)
 @test maximum(abs, value.(sol1) - AA.\b) < sqrt(eps())
 
-# Try cholesky factorization
+#Try cholesky factorization
 F = cholesky(value.(AAt)) 
 sol2 = TrackingFloat.(F\value.(bt))
 @test maximum(abs, value.(sol2) - AA\b) < sqrt(eps())
 
-# Which method was able to work with smallest elements?
+#Which method was able to work with smallest elements?
 getmax.(sol1)
 getmax.(sol2)
 
