fe6e173759f5d055835a4bca12153cf716c570e7
pantonomia <114500973+pantonomia@users.noreply.github.com>
2022-09-29 09:21:29 +0200

    Update TrackingFloat_Def.jl

--- src/TrackingFloat_Def.jl
+++ src/TrackingFloat_Def.jl
@@ -11,37 +11,91 @@ end
  
 aux = 0.0 
 
-#constructor
+#Constructor
+aux = 0.0 
 TrackingFloat() = TrackingFloat(aux, aux) 
 TrackingFloat(x) = TrackingFloat(x, aux)  
+TrackingFloat(v::TrackingFloat) = v
 
-#methods
-suma(v1::TrackingFloat, v2::TrackingFloat) = v1.new + v2.new
-mult(v1::TrackingFloat, v2::TrackingFloat) = v1.new * v2.new
-resta(v1::TrackingFloat, v2::TrackingFloat) = abs(v1.new - v2.new) 
-divide(v1::TrackingFloat, v2::TrackingFloat) = v1.new * (1/v2.new) 
-den(v1::TrackingFloat, v2::TrackingFloat) = (1/v2.new) 
-compara(v1::TrackingFloat, v2::TrackingFloat) = max(abs(v1.new), abs(v2.new), abs(v1.big), abs(v2.big))
+#Basic methods +, -, *, /
 
-#Operators defined for our new type
-a::TrackingFloat + b::TrackingFloat = TrackingFloat(suma(a,b), compara(a,b))
-a::TrackingFloat * b::TrackingFloat = TrackingFloat(mult(a,b), compara(a,b))
-a::TrackingFloat - b::TrackingFloat = TrackingFloat(resta(a,b), compara(a,b)) 
-a::TrackingFloat / b::TrackingFloat = TrackingFloat(divide(a,b), den(a,b))
+function +(v1::TrackingFloat, v2::TrackingFloat)
+    a = v1.new + v2.new
+    b = max(abs(v1.new), abs(v2.new),abs(v1.big), abs(v2.big))
+    return TrackingFloat(a,b)
+end
 
+function -(v1::TrackingFloat, v2::TrackingFloat)
+    a = v1.new-v2.new
+    b = max(abs(v1.new), abs(v2.new),abs(v1.big), abs(v2.big))
+    return TrackingFloat(a,b)
+end
+
+function *(v1::TrackingFloat, v2::TrackingFloat)
+    a = v1.new*v2.new
+    b = max(abs(v1.new), abs(v2.new),abs(v1.big), abs(v2.big))
+    return TrackingFloat(a,b)
+end
+
+function /(v1::TrackingFloat, v2::TrackingFloat)
+    a = v1.new / v2.new
+    if v2.new > 1.0
+        b = max(abs(v1.new), abs(v2.new),abs(v1.big), abs(v2.big))
+    else
+        b = max(abs(v1.new), 1/abs(v2.new),abs(v1.big), abs(v2.big))
+    end
+    return TrackingFloat(a,b)
+end
+
+#Basic methods 2 sqrt, <, -a, abs 
+
+function sqrt(v::TrackingFloat)
+    root = sqrt(v.new)
+    max_val = max(abs(v.new), abs(v.big))
+    return TrackingFloat(root, max_val)
+end
+
+function <(v1::TrackingFloat,v2::TrackingFloat)
+    if v1.new < v2.new
+        w = v1.new
+    else
+        w = v2.new
+    return w
+    end
+end
+
+function -(v::TrackingFloat)
+    a = -(v.new)
+    b = max(abs(v.new), abs(v.big))
+    return TrackingFloat(a,b)
+end
+
+function abs(v::TrackingFloat)
+    a = abs(v.new)
+    b = abs(v.big)
+    return TrackingFloat(a,b)
+end
+    
+#Additional basic operations
 value(v::TrackingFloat) = v.new
 getmax(v::TrackingFloat) = v.big
-sqrt(v::TrackingFloat) = TrackingFloat(sqrt(abs(v.new)), v.big)
 zero(v::TrackingFloat) = TrackingFloat(0.0, 0.0)
 TrackingFloat(v::TrackingFloat) = v
+    
+#promote rule
+    
 promote_rule(::Type{T}, ::Type{TrackingFloat}) where {T<:Number} = TrackingFloat
-
-# Try working with matrices e.g.: A = randn(10,10); b = randn(10) and convert using broadcast
-#At = TrackingFloat.(A) bt = TrackingFloat.(b) Try some operations e.g.: v = A*b; At; bt; vt = At*bt 
-# Get the max fields using a function getmax
-
-getmax(v::Vector{TrackingFloat}) = maximum(compara.(v,v))      #getmax.(vt)
-getmax(A::Matrix{TrackingFloat}) = maximum(compara.(A,A)) 
-
-
-
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
